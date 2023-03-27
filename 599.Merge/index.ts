540bc506cdd3521931b4b5fdf05bde81eac38011
xingrujiang <xingrujiang@tencent.com>
2023-03-27 18:01:24 +0800

    update exercise

--- 599.Merge/index.ts
+++ 599.Merge/index.ts
@@ -9,7 +9,7 @@ type coo = {
 }
 
 // &不能复写属性，复写属性只能用接口，Pick无法挑选不存在泛型里面的属性
-type Merge<T extends object,U extends object> = Omit<T,keyof U>&Omit<U,keyof T>&Pick<U,Exclude<keyof U,Omit<U,keyof T>>>
+type Merge<T extends object,U extends object> = Omit<T,keyof U>&U
 
 type Result = Merge<foo,coo>; // expected to be {name: string, age: number, sex: string}
 
